32289ad0454b77cde9cf0fb0c91d7a3f94ac2830
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-10-31 16:32:02 -0600

    implemented test for deleteCity -- not implemented in CustomList yet

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,4 +13,21 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDeleteCity(){
+        // To test deletion of cities, we first create a city to delete
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "Alberta");
+        list.addCity(calgary);
+        // Test if it got added correctly
+        assertTrue(list.hasCity(calgary));
+
+        // Now we delete
+
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+        // now we commit, we haven't yet implemented deleteCity so the test should fail
+
+    }
+
 }
